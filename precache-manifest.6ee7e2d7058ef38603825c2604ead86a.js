1850594937895bc159442d9387c2494e0e06a816
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-14 16:38:42 -0700

    Updates

---
+++ precache-manifest.6ee7e2d7058ef38603825c2604ead86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dbf3130d36e11212476a24c4d6c5c0",
+    "url": "/FoodReact/index.html"
+  },
+  {
+    "revision": "be1aa023d84dd7686659",
+    "url": "/FoodReact/static/css/main.30dd39c4.chunk.css"
+  },
+  {
+    "revision": "54268b0b99597d58fe9c",
+    "url": "/FoodReact/static/js/2.06bc94b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FoodReact/static/js/2.06bc94b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1aa023d84dd7686659",
+    "url": "/FoodReact/static/js/main.68401d7c.chunk.js"
+  },
+  {
+    "revision": "8d0559f5c17de6db404c",
+    "url": "/FoodReact/static/js/runtime-main.a65c9b18.js"
+  }
+]);
